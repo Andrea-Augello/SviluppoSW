62c0eee06eca8a948e67e4e2c6660216c8238f9b
Andrea-Augello <augelloandrea98@gmail.com>
2019-01-06 13:15:18 +0100

    Predisposta schermata principale medico

--- src/Oggetti/MainScreenMedico.java
+++ src/Oggetti/MainScreenMedico.java
@@ -1,4 +1,21 @@
 package Oggetti;
 
+import javax.swing.*;
+
 public class MainScreenMedico extends MainScreen {
+    private JList list1;
+    private JPanel panel;
+
+    public MainScreenMedico() {
+        JFrame frame = new JFrame("SPRINT");
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
